06b9598123df1b5e49874a1be3821b5dc979c6cc
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 04:31:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,8 @@ def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
     katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
+
   end
 end
