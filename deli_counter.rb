0f7042fe87b65a4f8ffac41432c7a5ed63cf3d49
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 04:45:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli, name)
   end
 end
 
-def now_serving(katz_deli)
+def now_serving(array)
   if katz_deli.length==0
     puts "There is nobody waiting to be served!"
   else
