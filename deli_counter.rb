683cf11eefe8d222405fe0d60624d7abcab8c525
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 04:31:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,5 @@ def take_a_number(katz_deli, name)
   else
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
-    katz_deli.each_with_index do |name , index|
-
-    end
   end
 end
