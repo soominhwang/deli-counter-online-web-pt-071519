f469934e039aadc1adcb5628b7b6da2322025d80
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 04:24:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line(array)
+  if array.length==0
+    puts "The line is currently empty."
+  else
+    line_array = []
+    array.each_with_index do |name, index|
+      line_array << "#{index+1}. #{name}"
+    end
+    puts "The line is currently: #{line_array.join(' ')}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length==0
+    katz_deli << name
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    katz_deli << name
+    katz_deli.each_with_index do |name , index|
+      puts "Welcome, #{name}. You are number #{index+1} in line."
+    end
+  end
+end
