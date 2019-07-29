4c0ddc5beeea079a78730d789e5a676abe7ee5d5
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 04:39:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,8 @@ def now_serving(katz_deli)
   if katz_deli.length==0
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.first
-    puts "Currently serving #{katz_deli.first}".
+    katz_deli.first = name
+    puts "Currently serving #{name}".
     katz_deli.pop(katz_deli.first)
   end
 end
